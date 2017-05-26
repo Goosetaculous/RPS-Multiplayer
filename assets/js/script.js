252db5a2433bd822e7ae9b9505680bbf960a4155
Goose <josephtrop@gmail.com>
2017-05-26 16:43:20 -0700

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyDzKCP5UzFSGIdmSq74yBdYUI3_CY-rSGo",
@@ -8,7 +9,8 @@ $(document).ready(function(){
         storageBucket: "goosetaculous.appspot.com",
         messagingSenderId: "325415828804"
     };
-    var openSit,choice1,choice2
+    var openSit,choice1,choice2,winner
+    var name1,name2
 
 
     firebase.initializeApp(config);
@@ -21,6 +23,24 @@ $(document).ready(function(){
     database.ref().on("value",function(snapshot) {
        // var sessions = Object.keys(snapshot.val()).length ? Object.keys(snapshot.val()).length : null
         if(openSit){
+
+
+
+            database.ref(openSit+"/1/msg").on("value",function (chat) {
+                var conversation = $("#talk-shit").val().trim() + "\n"
+                $("#talk-shit").text(conversation + chat.val()+ " <--" + name1)
+                scrollBottom()
+
+            })
+
+            database.ref(openSit+"/2/msg").on("value",function (chat) {
+                var conversation = $("#talk-shit").val().trim() + "\n"
+                $("#talk-shit").text(conversation + chat.val() + " <--" + name2)
+                scrollBottom()
+
+            })
+
+
             database.ref(openSit+"/turn").once("value",function (data) {
                 turn = data.val()
             })
@@ -30,13 +50,15 @@ $(document).ready(function(){
             database.ref(openSit+"/2/choice").once("value",function(c2){
                 choice2 = c2.val()
             })
-
-            if (choice1 && choice2){
-                var winner = eveluateWinner(choice1,choice2)
+            if (choice1 && choice2 && turn == 1){
+                winner = eveluateWinner(choice1,choice2)
 
                 $(".rps1").hide()
-                setTimeout( clearResults(winner),5000)
+                setTimeout( clearResults,2000,winner)
+
             }
+
+
         }
         for (session in snapshot.val()) {
             if (openSession(session) ) {
@@ -47,38 +69,89 @@ $(document).ready(function(){
     }, function(errorObject){
         console.log("error",errorObject)
     });
+
+    function scrollBottom(){
+        $("#talk-shit").animate({
+            scrollTop:$("#one")[0].scrollHeight - $("#one").height()
+        },1000)
+    }
+
+
+
+
+
     /**
-     * Clear the choices after 5 seconds
+     * Update winner loose stats
+     */
+    function updatedStats(winner){
+        var wins,loose
+        if(winner ==1){
+            //update winner
+            database.ref(openSit+"/1/wins").once("value",function (data) {
+                wins = data.val()
+                wins++
+            })
+            database.ref(openSit+"/1/wins").set(wins)
+            //update loose
+            database.ref(openSit+"/2/loses").once("value",function (data) {
+                loose = data.val()
+                loose++
+            })
+            database.ref(openSit+"/2/loses").set(loose)
+        }
+        //
+        //     //update winner
+        //     database.ref(openSit+"/2/wins").on("value",function (data) {
+        //         wins = data.val()
+        //         wins++
+        //     })
+        //     database.ref(openSit+"/2/wins").set(wins)
+        //     //update loose
+        //     database.ref(openSit+"/1/loses").on("value",function (data) {
+        //         loose = data.val()
+        //         loose++
+        //     })
+        //     database.ref(openSit+"/1/loses").set(loose)
+        // }
+    }
+
+    /**
+     * Clear the choices after 3 seconds
      */
     function clearResults(winner) {
-        var wins
-        //clear all the choices
-        database.ref(openSit+"/1/choice").set("")
-        database.ref(openSit+"/2/choice").set("")
-        //update the win lose records
-        if ( winner == 1 ){
-            database.ref(openSit+"/1/wins").once("value", function(data){
-                wins =  data.val()
-            })
-            wins++
-            database.ref(openSit+"/1/wins").set(wins)
-        }
-
-
-
-
-
+        //console.log("->",winner)
+        database.ref(openSit + "/1/choice").set("")
+        database.ref(openSit + "/2/choice").set("")
         $(".game-result").html("")
-        if( sessionStorage.getItem("player") == 1){
+        if (sessionStorage.getItem("player") == 1) {
             $(".rps1").show()
         }
+        if (winner == 1) {
+            //update winner
+            database.ref(openSit + "/1/wins").once("value", function (data) {
+                wins = data.val()
+                wins++
+            })
+            database.ref(openSit + "/1/wins").set(wins)
+            //update loose
+            database.ref(openSit + "/2/loses").once("value", function (data) {
+                loose = data.val()
+                loose++
+            })
+        }
     }
+
+
+
+
+
     /**
      * Populate Player 1 Name
      */
     database.ref(openSit).on("child_added",function(data){
         if(data.val()[1]){
             $("#player1").html(data.val()[1].name)
+            name1=data.val()[1].name
         }
     })
     /**
@@ -87,9 +160,9 @@ $(document).ready(function(){
     database.ref(openSit).on("child_changed",function(data){
         if(data.val()[2]){
             $("#player2").html(data.val()[2].name)
+            name2=data.val()[2].name
         }
     })
-
     /**
      *Set player 1 choice
      */
@@ -168,15 +241,17 @@ $(document).ready(function(){
         var obj
         // ASSIGN PLAYER 1
         if (openSit === undefined && playerName){
+
             sessionStorage.setItem("player",1)
-            obj = {1: {"wins":0, "loses":0, "name":playerName,"choice":""}}
+            obj = {1: {"wins":0, "loses":0, "name":playerName,"choice":"","msg":""}}
             database.ref().push(obj)
             $(".player-input").hide()
         }
         // ASSIGN PLAYER 2
         else if( playerName ){
+
             sessionStorage.setItem("player",2)
-            obj ={"wins":0,"loses":0,"name":playerName,"choice":""}
+            obj ={"wins":0,"loses":0,"name":playerName,"choice":"","msg":""}
             database.ref(openSit+"/"+2).set(obj)
             database.ref(openSit+"/turn").set(1)
             $(".player-input").hide()
